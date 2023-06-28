e7ef66483ec1ad82aff7fd418c0abfb9aa9df625
grivaz <cgrivaz@protonmail.com>
2023-06-28 17:34:20 -0400

    add deep learning capabilities

--- annotation_helper.py
+++ annotation_helper.py
@@ -25,17 +25,18 @@ import time
 import os
 import get_genes
 import csv
+import deep_learning
 
-parser = argparse.ArgumentParser(description="Gets gene candidates from xml papers")
-parser.add_argument("input", type=argparse.FileType('r'), help="A text file containing one pmid per line")
-args = parser.parse_args()
+arg_parser = argparse.ArgumentParser(description="Gets gene candidates from xml papers")
+arg_parser.add_argument("input", type=argparse.FileType('r'), help="A text file containing one pmid per line")
+cmd_args = arg_parser.parse_args()
 
-config = configparser.ConfigParser()
-config.read("config/config.ini")
+config_parser = configparser.ConfigParser()
+config_parser.read("config/config.ini")
 
 #get pmid to pmcid pickle dictionary
-with open(config.get('PICKLES','PMC_ids_dict'), "rb") as f:
-    pmid_to_pmcid = pickle.load(f)
+with open(config_parser.get('PICKLES','PMC_ids_dict'), "rb") as f:
+    pmid_to_pmcid_dict = pickle.load(f)
 
 def getFtpPath(pmcid: str):
     """Returns the ftp path to a paper given its pmcid
@@ -44,9 +45,10 @@ def getFtpPath(pmcid: str):
         pmcid, str
             The pmcid of the paper
     """
-    api_url = "https://www.ncbi.nlm.nih.gov/pmc/utils/oa/oa.fcgi?id=" + pmcid
-    response = requests.get(api_url)
+    pmc_api_url = "https://www.ncbi.nlm.nih.gov/pmc/utils/oa/oa.fcgi?id=" + pmcid
+    response = requests.get(pmc_api_url)
     dict_data = xmltodict.parse(response.content)
+    ftplink = ""
     if "error" in dict_data['OA']:
         print("ERROR: " + dict_data['OA']['error']['@code'] + " " + pmcid, file=sys.stderr)
     else:
@@ -56,7 +58,7 @@ def getFtpPath(pmcid: str):
         else:
             ftplink = link['@href']
         assert (".tar.gz" in ftplink)
-    time.sleep(config.getint('PARAMETERS','sleep_time_between_requests'))
+    time.sleep(config_parser.getint('PARAMETERS','sleep_time_between_requests'))
     return ftplink
 
 def download(ftp: str):
@@ -66,7 +68,7 @@ def download(ftp: str):
         ftp, str
             The ftp path to the paper
     """
-    wget = f"wget -nc --timeout=10 -P {config.get('PATHS', 'corpus')} {ftp}"
+    wget = f"wget -nc --timeout=10 -P {config_parser.get('PATHS', 'corpus')} {ftp}"
     os.system(wget)
 
 def getXmlFromTar(pmcid: str):
@@ -76,13 +78,13 @@ def getXmlFromTar(pmcid: str):
         pmcid, str
             The pmcid of the paper
     """
-    f = f"{config.get('PATHS', 'corpus')}/{pmcid}.tar.gz"
-    untar = "tar -xf " + f + " -C " + config.get('PATHS', 'corpus')
+    f = f"{config_parser.get('PATHS', 'corpus')}/{pmcid}.tar.gz"
+    untar = "tar -xf " + f + " -C " + config_parser.get('PATHS', 'corpus')
     os.system(untar)
     # make xml directory if it doesn't exist
-    if not os.path.exists(config.get('PATHS', 'xml')):
-        os.makedirs(config.get('PATHS', 'xml'))
-    copy_xml = "cp " + config.get('PATHS', 'corpus') + "/" + pmcid + "/" + "*.nxml " + config.get('PATHS', 'xml') \
+    if not os.path.exists(config_parser.get('PATHS', 'xml')):
+        os.makedirs(config_parser.get('PATHS', 'xml'))
+    copy_xml = "cp " + config_parser.get('PATHS', 'corpus') + "/" + pmcid + "/" + "*.nxml " + config_parser.get('PATHS', 'xml') \
                + "/" + pmcid + ".nxml"
     os.system(copy_xml)
 
@@ -93,69 +95,88 @@ def removeFiles(pmcid: str):
         pmcid, str
             The pmcid of the paper
     """
-    f = f"{config.get('PATHS', 'corpus')}/{pmcid}.tar.gz"
+    f = f"{config_parser.get('PATHS', 'corpus')}/{pmcid}.tar.gz"
     try:
         os.remove(f)
     except FileNotFoundError:
         pass
-    f = f"{config.get('PATHS', 'corpus')}/{pmcid}"
+    f = f"{config_parser.get('PATHS', 'corpus')}/{pmcid}"
     # remove extracted directory
     os.system("rm -r " + f)
-    f = f"{config.get('PATHS', 'xml')}/{pmcid}.nxml"
+    f = f"{config_parser.get('PATHS', 'xml')}/{pmcid}.nxml"
     try:
         os.remove(f)
     except FileNotFoundError:
         pass
 
 #unpickle gene dictionary
-with open(config.get('PICKLES','gene_dict'), "rb") as f:
+with open(config_parser.get('PICKLES','gene_dict'), "rb") as f:
     gene_dict = pickle.load(f)
 
+#unpickle fbid to symbol dictionary
+with open(config_parser.get('PICKLES','fbid_to_symbol_dict'), "rb") as f:
+    fbid_to_symbol = pickle.load(f)
+
+if config_parser.getboolean('PARAMETERS', 'use_deep_learning'):
+    deep_learning.initialize(config_parser.get('PATHS', 'deep_learning_model'))
+
 results = {}
-for pmid in args.input:
-    if pmid.strip() not in pmid_to_pmcid:
+for pmid in cmd_args.input:
+    if pmid.strip() not in pmid_to_pmcid_dict:
         # print it to the standard error stream
         print("no pmcid for " + pmid.strip(), file=sys.stderr)
     else:
-        pmcid = pmid_to_pmcid[pmid.strip()]
+        pmcid = pmid_to_pmcid_dict[pmid.strip()]
         ftp = getFtpPath(pmcid)
         download(ftp)
         getXmlFromTar(pmcid)
-        result = get_genes.get_genes(os.path.join(config.get('PATHS', 'xml'), pmcid + ".nxml"),
-                                  gene_dict, config.get('PARAMETERS', 'snippet_type'),
-                                  config.getboolean('PARAMETERS', 'output_gene_occurence'),
-                                  config.getboolean('PARAMETERS', 'output_gene_frequency'),
-                                  config.getboolean('PARAMETERS', 'output_word_frequency'),
-                                  config.getboolean('PARAMETERS', 'output_raw_occurence'))
+        result = None
+        if config_parser.getboolean('PARAMETERS', 'use_deep_learning'):
+            result = deep_learning.get_genes_with_dl(os.path.join(config_parser.get('PATHS', 'xml'), pmcid + ".nxml"), gene_dict, fbid_to_symbol)
+        else:
+            result = get_genes.get_genes(os.path.join(config_parser.get('PATHS', 'xml'), pmcid + ".nxml"),
+                                    gene_dict, config_parser.get('PARAMETERS', 'snippet_type'),
+                                    config_parser.getboolean('PARAMETERS', 'output_gene_occurence'),
+                                    config_parser.getboolean('PARAMETERS', 'output_gene_frequency'),
+                                    config_parser.getboolean('PARAMETERS', 'output_word_frequency'),
+                                    config_parser.getboolean('PARAMETERS', 'output_raw_occurence'))
         results[pmid.strip()] = result
-        if config.getboolean('PARAMETERS', 'remove_files'):
+        if config_parser.getboolean('PARAMETERS', 'remove_files'):
             removeFiles(pmcid)
 
 
-with open(config.get('PATHS', 'output'), 'w', newline='', encoding='utf-8') as f:
+with open(config_parser.get('PATHS', 'output'), 'w', newline='', encoding='utf-8') as f:
     writer = csv.writer(f, delimiter='\t', quotechar='"', quoting=csv.QUOTE_MINIMAL, escapechar='\\')
     # write data to file
-    for pmid in results:
-        confidences = results[pmid][0]
-        occurrences = results[pmid][1]
-        for fbgn in confidences:
-            scores = []
-            if get_genes.GENES in confidences[fbgn]:
-                scores.append(confidences[fbgn][get_genes.GENES])
-            if get_genes.WORD in confidences[fbgn]:
-                scores.append(confidences[fbgn][get_genes.WORD])
-            if get_genes.RAW in confidences[fbgn]:
-                scores.append(confidences[fbgn][get_genes.RAW])
-            snippet_type = config.get('PARAMETERS', 'snippet_type')
-            if snippet_type != 'none' and config.getboolean('PARAMETERS', 'output_gene_occurence'):
-                for i, occurrences_for_gene in enumerate(occurrences[fbgn]):
-                    genes_occurrence = occurrences_for_gene[0]
-                    snippet = occurrences_for_gene[1]
-                    writer.writerow([pmid, fbgn, genes_occurrence, snippet]+scores)
-            elif snippet_type != 'none' or config.getboolean('PARAMETERS', 'output_gene_occurence'):
-                for occurrence in occurrences[fbgn]:
-                    writer.writerow([pmid, fbgn, occurrence]+scores)
-            else:
-                writer.writerow([pmid,fbgn]+scores)
+    # if we're using deep learning
+    if config_parser.getboolean('PARAMETERS', 'use_deep_learning'):
+        for pmid in results:
+            for fbgn in results[pmid]:
+                #when using deep learning, we only output the pmid, fbgn, and confidence
+                writer.writerow([pmid, fbgn, results[pmid][fbgn]])
+
+    else:
+        for pmid in results:
+            confidences = results[pmid][0]
+            occurrences = results[pmid][1]
+            for fbgn in confidences:
+                scores = []
+                if get_genes.GENES in confidences[fbgn]:
+                    scores.append(confidences[fbgn][get_genes.GENES])
+                if get_genes.WORD in confidences[fbgn]:
+                    scores.append(confidences[fbgn][get_genes.WORD])
+                if get_genes.RAW in confidences[fbgn]:
+                    scores.append(confidences[fbgn][get_genes.RAW])
+                snippet_type = config_parser.get('PARAMETERS', 'snippet_type')
+                if snippet_type != 'none' and config_parser.getboolean('PARAMETERS', 'output_gene_occurence'):
+                    for i, occurrences_for_gene in enumerate(occurrences[fbgn]):
+                        genes_occurrence = occurrences_for_gene[0]
+                        snippet = occurrences_for_gene[1]
+                        writer.writerow([pmid, fbgn, genes_occurrence, snippet]+scores)
+                elif snippet_type != 'none' or config_parser.getboolean('PARAMETERS', 'output_gene_occurence'):
+                    for occurrence in occurrences[fbgn]:
+                        writer.writerow([pmid, fbgn, occurrence]+scores)
+                else:
+                    writer.writerow([pmid,fbgn]+scores)
 
 
---
+++ deep_learning.py
@@ -0,0 +1,65 @@
+# Copyright 2023 Charlie Grivaz
+ #
+ # This file is part of Fly Base Annotation Helper
+ #
+ # Fly Base Annotation Helper is free software: you can redistribute it and/or modify
+ # it under the terms of the GNU General Public License as published by
+ # the Free Software Foundation, either version 3 of the License, or
+ # (at your option) any later version.
+ #
+ # Fly Base Annotation Helper is distributed in the hope that it will be useful,
+ # but WITHOUT ANY WARRANTY; without even the implied warranty of
+ # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ # GNU General Public License for more details.
+ #
+ # You should have received a copy of the GNU General Public License
+ # along with Fly Base Annotation Helper. If not, see <http://www.gnu.org/licenses/>.
+from transformers import AutoTokenizer, AutoModelForSequenceClassification, pipeline
+import os
+import sys
+import pubmed_parser as pp
+import typing
+import get_genes
+
+
+model_pipeline = None
+tokenizer_kwargs = {'padding': "max_length", 'truncation': True, 'max_length': 512}
+def initialize(path_to_model):
+    global model_pipeline
+    if model_pipeline is None:
+        scibert = "allenai/scibert_scivocab_uncased"
+        tokenizer = AutoTokenizer.from_pretrained(scibert, model_max_length=512)
+        model = AutoModelForSequenceClassification.from_pretrained(path_to_model, num_labels=2)
+        model_pipeline = pipeline("text-classification", model=model, tokenizer=tokenizer, device=-1)
+
+def get_gene(fbrf, candidates, fbid_to_symbol):
+    occurrences = set()
+    for occurrence in candidates:
+        occurrences.add(occurrence)
+    out = fbid_to_symbol[fbrf] + " " + " ".join(str(candidates.count(cand)) + " " + cand for cand in occurrences)
+    return out
+ 
+def get_genes_with_dl(paper_file: str, gene_dict: typing.Dict[str, str], fbid_to_symbol: typing.Dict[str, str]):
+    if os.path.isfile(paper_file) and paper_file.endswith("nxml"):
+        # get text
+        pubmed_dict = pp.parse_pubmed_xml(paper_file)  # dictionary output
+        abstract = pubmed_dict["abstract"]  # abstract
+        _, candidates = get_genes.get_genes(paper_file, gene_dict, 'none', True, False, False, False)
+        results = {}
+        for fbrf in candidates:
+            gene = get_gene(fbrf, candidates[fbrf], fbid_to_symbol)
+            input = gene + ". " + abstract
+            results[fbrf] = transform(input)
+        return results
+    else:
+        print("File does not exist or is not an nxml file: " + paper_file, file=sys.stderr)
+
+def transform(input):
+    #print error if model_pipeline is not initialized
+    if model_pipeline is None:
+        raise Exception("model_pipeline not initialized")
+    result = model_pipeline(input, **tokenizer_kwargs)
+    if result[0]["label"] == "LABEL_1":
+        return result[0]["score"]
+    else:
+        return 1 - result[0]["score"]
